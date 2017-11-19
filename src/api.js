dcd2fb46366e1fba96d467900e8e8d098eba3143
Chanon Roy <chanonr@surveymonkey.com>
2017-11-19 00:35:14 -0500

    need to traverse comments, setup api

--- src/api.js
+++ src/api.js
@@ -22,23 +22,25 @@ export const INSTA_API = {
         return `https://api.instagram.com/oauth/authorize?client_id=${service.client_id}&redirect_uri=${service.redirect_uri}&response_type=token`
     },
 
-    /** (PUBLIC) GET a random comment from a media object
+    /** (PUBLIC) GET the media_id and total comment count
      * @param { String } access_token       OAuth token given after auth
-     * @param { String } media_id           unique identifier for the media object 
+     * @param { String } shortcode          unique shortcode identifier found in a media url  
     */
-    get_random_comment(access_token, media_id) {
+    get_media_data: (access_token, shortcode) =>
+        request.get(`/media/shortcode/${shortcode}`),
 
-        // (REQUEST) Get total number of comments
-       
-            // Generate random number for comment page
-
-            // (REQUEST) Get a specific comment page
-
-                // Select a random comment
-
-                // (REQUEST) - get information on that user for display + follow back param
-
-        // TODO: try and reduce request count from 3
-    },
+    /** (PUBLIC) GET a page of comments
+     * @param { String } access_token       OAuth token given after auth
+     * @param { String } media_id           unique identifier for the media object
+    */
+    get_comments: (access_token, media_id) =>
+        request.get(`/media/${media_id}/comments`),
+    
+    /** (PUBLIC) GET the relationship between the oauth user and user_id
+     * @param { String } access_token       OAuth token given after auth
+     * @param { String } user_id            unique identifier for the desired user to check against
+    */
+    get_relationship: (acess_token, user_id) =>
+        request.get(`/user/${user_id}/relationship`)
 
 }
---
+++ src/views/Roll.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class Roll extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+         <div> Hello World </div>   
+        )
+    }
+
+}
